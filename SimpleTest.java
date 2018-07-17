468fe2bc7eb3e7ee19ccd385c861d89bd5d384a8
Tahar-Analyst <tahaferd@yahoo.fr>
2018-07-17 16:16:08 +0100

    SimpleTest.java

---
+++ SimpleTest.java
@@ -0,0 +1,16 @@
+package ScriptsSahi;
+
+public class SimpleTest{
+
+	public static void main(String[] args){
+		
+
+		//for(int i=1;1<=10;i++){
+			
+			System.out.print("Hello Word...");
+			
+			System.exit(0);
+			
+	  }
+	 
+}
